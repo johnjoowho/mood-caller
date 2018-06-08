aa7a61eca014a0ddcdb9b8d7ab86b0a779e78afb
John Joo <john@Johns-iMac.local>
2018-06-07 17:24:00 -0700

    added users directory

--- server.js
+++ server.js
@@ -6,4 +6,31 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; 
 
 const { DATABASE_URL, PORT } = require('./config'); 
-const { }
+const { MoodEntry } = require('./models'); 
+
+const app = express(); 
+
+app.use(morgan('common')); 
+app.use(express.json()); 
+
+app.get('/posts', (req, res) => { 
+  MoodEntry 
+    .find()
+    .then(entries => { 
+      res.json(entries.map(entry => entry.serialize())); 
+    })
+    .catch(err =>  {
+      console.error(err); 
+      res.status(500).json({ error: 'something went wrong' }); 
+    }); 
+}); 
+
+app.get('/entries/:id', (req, res) => { 
+  MoodEntry 
+    .findById(req.params.id)
+    .then(entry => res.json(entry.serialize())); 
+    .catch(err => { 
+      console.error(err); 
+      res.status(500).json({ error: 'something went wrong' }); 
+    }); 
+}); 
