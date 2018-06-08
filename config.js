c20477fd60c266ca7899718e8fabb780a12fc32c
John Joo <john@Johns-iMac.local>
2018-06-08 11:41:45 -0700

    added jwt-secret

--- config.js
+++ config.js
@@ -2,3 +2,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/moods-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-moods-app';
 exports.PORT = process.env.PORT || 8080;
+
+exports.JWT_SECRET = process.env.JWT_SECRET || 'test'; 
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
