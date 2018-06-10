a80769c106428f34344d24b287f324931c0dcc52
John Joo <john@Johns-iMac.local>
2018-06-10 12:45:32 -0700

    fixed typo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static('public'));
 
 //CORS cross-origin resource sharing
 app.use(function(req, res, next) { 
-  res.hearder('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); 
   if (req.method=='OPTIONS') { 
