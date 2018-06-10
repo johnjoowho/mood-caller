e16c24b6f7ad19a6f078a1c43caab848f11f48a2
John Joo <john@Johns-iMac.local>
2018-06-10 14:58:17 -0700

    added router js functions;

--- entries/router.js
+++ entries/router.js
@@ -30,4 +30,61 @@ router.get('/:id', (req, res) => {
     }); 
 }); 
 
+router.post('/', (req, res) => {
+  const requiredFields = ['rating', 'description', 'created']; 
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing '${field}' in request body`
+      console.log(message); 
+      return res.status(400).send(message);
+    }
+  }
+
+  MoodEntry 
+    .create({
+      rating: req.body.rating, 
+      descriptiong: req.body.description, 
+      created: request.body.created}) 
+    .then(
+      moodentry => res.status(201).json(moodentry.serialize()))
+    .catch(err => {
+      console.error(err); 
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` + 
+      `(${req.body.id}) must match`
+    );
+    console.error(message); 
+    //return to break out of function
+    return res.status(400).json({message: message});
+  }
+
+  const toUpdate = {}; 
+  const updateableFields = ['rating', 'description', 'created']; 
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  MoodEntry 
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(moodentry => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+router.delete('/:id', (req, res) => {
+  MoodEntry
+    .findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
 module.exports = {router}; 
--- public/index.js
+++ public/index.js
@@ -245,7 +245,7 @@ function handleLogoutSubmit() {
 }
 
 function logout() { 
-
+  
 }
 
 function displaySignupPage() {
