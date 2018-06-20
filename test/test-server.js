b4c65470b6a4948c04453fe04ffc32876c9bad9e
John Joo <john@Johns-iMac.local>
2018-06-20 11:47:24 -0700

    added test db url to testserver.js

--- test/test-server.js
+++ test/test-server.js
@@ -3,7 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { closeServer, runServer, app } = require('../server'); 
-
+const { TEST_DATABASE_URL } = require('../config'); 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
