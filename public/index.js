ea8746255d131779f175ee07927765733811c3a4
John Joo <john@Johns-iMac.local>
2018-06-20 10:19:26 -0700

    added test

--- public/index.js
+++ public/index.js
@@ -34,7 +34,7 @@ const MoodEntries = {
     callback(Object.keys(this.items).map(key => this.items[key])); 
   },
 
-  delete: function(id) { 
+  delete: function(id, callback) { 
     console.log('Deleting mood entry');
     delete this.items[id]; 
     callback(); 
@@ -46,7 +46,7 @@ const MoodEntries = {
     if (!(id in this.items)) { 
       throw StorageException(`Can't update item ${id} because it doesn't exist`)
     }
-    this.items[updatedItem.id] = updateItem; 
+    this.items[updatedItem.id] = updatedItem; 
     callback(updatedItem); 
   }
 }
@@ -63,38 +63,72 @@ function sortByCreated(firstDate, secondDate) {
   return firstDate > secondDate ? -1: firstDate < secondDate ? 1: 0
 }
 
-function getMostRecent(moods, count=5) { 
+function getMostRecent(moods, count=7) { 
   return moods.sort(sortByCreated).slice(0, count); 
 }
 
 function generateGraph(moods) {
-  const ctx = $('#graph-chart').getContext('2d');
+  const ctx = $('#graph-chart')[0].getContext('2d');
+  const options = {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit'};
   const chartData = {
     type: 'line', 
-    labels: moods.map(function(mood) {
-      return mood.created.toLocaleDateString('en-US')
-    }),
+    data: {
+      
+      labels: moods.map(function(mood) {
+        const created = new Date(mood.created); 
+        return created.toLocaleDateString('en-US', options)
+      }),
 
-    datasets: [{
-      label: 'Rating', 
-      data: moods.map(function(mood) {
-        return mood.rating
-      })
-    }]
-  }
+      datasets: [{
+        label: 'Rating', 
+        data: moods.map(function(mood) {
+          return mood.rating
+        })
+      }],
+    },
 
-  const myChart = new Chart (ctx, chartData); 
+    backgroundColor: [
+      
+    ],
+
+    options: {
+      layout: {
+        padding: {
+          left: 50,
+          right: 50,
+          top: 50, 
+          bottom: 50
+        }
+      },
+
+      events: ['click'], 
+
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+        }
+      }
+    }
+
+  return new Chart (ctx, chartData); 
 }
 
 function generateProfilePage(moods) {
   const mostRecent = getMostRecent(moods); 
 
   const buttonHtml = `
-    <button id="create-new-mood">click to log a new mood</button>
-    <button id="log-out">log out</button> 
+    <div id="buttonhtml">
+      <button id="create-new-mood">click to log a new mood</button>
+      <button id="log-out">log out</button> 
+    </div>
   ` 
   const emptyHtml = `
-    <p class="empty-message">Sorry, there are no entries</p> 
+    <div id="emptyhtml">
+      <p class="empty-message">Sorry, there are no entries</p> 
+    </div> 
   `
 
   const graphHtml = `
@@ -104,15 +138,28 @@ function generateProfilePage(moods) {
   </div>
 </section>
   `
+  const options = {weekday:'long', year:'numeric', month:'short', day:'2-digit', hour:'2-digit', minute:'2-digit'};
 
   const profileHtml = `
-  <section id="profile"> 
-      <div class="info-container">
-        <p>placeholder</p>
-      </div>
-      <div class="history container">
-        <p>${mostRecent}</p>
-      </div>
+    <section id="profile"> 
+      <h2>Your most recent entries</h2> 
+        <ol class="history-container">
+          ${mostRecent.map(function(recentItem) {
+            const created = new Date(recentItem.created); 
+            return `
+              <li>
+                <ul class="mood-entry">
+                  <li>Date: ${created.toLocaleDateString('en-US', options)}</li>
+                  <li>Your mood was: ${recentItem.rating}</li>
+                  <li>Entry: ${recentItem.description}</li>
+                  <button data-id="${recentItem.id}" id="delete-entry">Delete</button>
+                  <button data-id="${recentItem.id}" id="edit-entry">Edit</button>
+                </ul>
+              </li>
+            `
+          }).join('')
+          }
+        </ol>
     </section> 
   `
 
@@ -125,47 +172,109 @@ function generateProfilePage(moods) {
   `
 }
 
-function handleMoodSubmit(event) { 
+function handleShowLoginButton() {
+  $('main').on('click', '#showLoginButton', function(event) { 
+
+    displayLoginPage();
+  });  
+} 
+
+function handleEdit() {
+  $('main').on('click', '#edit-entry', function(event) {
+    const thisId = $(event.currentTarget).data('id');
+
+    displayMoodField(STORE.items[thisId]); 
+  });
+}
+
+function handleDelete() {
+    $('body').on('click', '#delete-entry', function(event) { 
+    const thisId = $(event.currentTarget).data('id'); 
+    
+    STORE.delete(thisId, displayLandingPage); 
+  });
+}
+
+function handleSignupSubmit() { 
+  $('main').on('submit', '.signup-form', function(event) { 
+    event.preventDefault(); 
+    const username = $('#newusername').val(); 
+    const password = $('#newpassword').val(); 
+    const newUser = {username, password}; 
+    addNewUser(newUser, displayLandingPage); 
+  });
+}
+
+function handleMoodSubmit() { 
   $('main').on('submit', '.new-mood-form', function(event) {
     event.preventDefault(); 
     const description = $('#mood-describe').val(); 
     const rating = $('input[name=mood]:checked').val(); 
-    const mood = {rating, description}
-    STORE.create(mood, displaySuccessPage); 
-  })
+    const mood = {rating, description};
+    const thisId = $(event.currentTarget).data('id') || null; 
+    const thisCreated = $(event.currentTarget).data('created') || null; 
+    if (thisId && thisCreated) {
+      mood.id = thisId; 
+      mood.created = thisCreated; 
+      STORE.update(mood, displayLandingPage); 
+    } else {STORE.create(mood, displaySuccessPage);} 
+  });
 }
 
-function generateMoodInput() {
+function handleGotoProfileSubmit() {
+  $('main').on('click', '#go-to-profile', function(event) {
+    getMoodsFromApi(displayProfilePage);
+  }); 
+}
+
+function handleShowNewMoodButton() {
+  $('main').on('click', '#create-new-mood', function(event) {
+    displayMoodField(); 
+  });
+}
+
+function handleLogoutSubmit() {
+  $('main').on('click', '#log-out', function(event) { 
+    logout(); 
+  });
+}
+
+function generateMoodInput(mood = null) {
+  if (mood) console.log(mood.rating);
   return `
-    <form class="new-mood-form">
-      <label for="mood-description">Tell me what's up?</label>
-      <input type="text" name="mood-description" id="mood-describe" placeholder="e.g., Just had bad coffee" required>
+    <form id="new-mood-form" ${mood ? `data-id="${mood.id}" data-created="${mood.created}"` : ''}>
+        <fieldset> 
+          <legend>Mood Entry Form</legend> 
+            <label for="mood-description">Tell me what's up?</label>
+            </br> 
+            <input type="text" name="mood-description" ${mood ? `value="${mood.description}"` : ''} id="mood-describe" placeholder="e.g., Just had bad coffee" required>
 
-      <div class="rating-container">
-        <div class="mood-choices">
-          <input type="radio" name="mood" id="choice-1" value="1" required> 
-          <label for="choice-1">I feel horrible pain/sickness</label>
-
-          <input type="radio" name="mood" id="choice-2" value="2"> 
-          <label for="choice-2">I feel down and out</label>
-
-          <input type="radio" name="mood" id="choice-3" value="3"> 
-          <label for="choice-3">I feel lethargic</label>
-
-          <input type="radio" name="mood" id="choice-4" value="4"> 
-          <label for="choice-4">I feel ok</label>
-
-          <input type="radio" name="mood" id="choice-5" value="5"> 
-          <label for="choice-5">I feel good</label>
-
-          <input type="radio" name="mood" id="choice-6" value="6"> 
-          <label for="choice-6">I feel like going out</label>
-
-          <input type="radio" name="mood" id="choice-7" value="7"> 
-          <label for="choice-7">I feel like I can leap for joy</label>
-        </div>  
-      </div> 
-      <button type="submit" id="submit-new-mood">Add mood</button>  
+            <div class="rating-container">
+              <div class="mood-choices">
+                <input type="radio" name="mood" id="choice-1" class="choices" value="1" required ${mood ? mood.rating == 1? 'checked' :'' : ''}> 
+                <label for="choice-1">I feel horrible pain/sickness</label>
+                </br> 
+                <input type="radio" name="mood" id="choice-2" class="choices" value="2" ${mood ? mood.rating == 2? 'checked' :'' : ''}> 
+                <label for="choice-2">I feel down and out</label>
+                </br>
+                <input type="radio" name="mood" id="choice-3" class="choices" value="3" ${mood ? mood.rating == 3? 'checked' :'' : ''}> 
+                <label for="choice-3">I feel lethargic</label>
+                </br>       
+                <input type="radio" name="mood" id="choice-4" class="choices" value="4" ${mood ? mood.rating == 4? 'checked' :'' : ''}> 
+                <label for="choice-4">I feel ok</label>
+                </br> 
+                <input type="radio" name="mood" id="choice-5" class="choices" value="5" ${mood ? mood.rating == 5? 'checked' :'' : ''}> 
+                <label for="choice-5">I feel good</label>
+                </br>
+                <input type="radio" name="mood" id="choice-6" class="choices" value="6" ${mood ? mood.rating == 6? 'checked' :'' : ''}> 
+                <label for="choice-6">I feel like going out</label>
+                </br>
+                <input type="radio" name="mood" id="choice-7" class="choices" value="7" ${mood ? mood.rating == 7? 'checked' :'' : ''}> 
+                <label for="choice-7">I feel like I can leap for joy</label>
+              </div>  
+            </div> 
+        </fieldset> 
+      <button type="submit" id="submit-new-mood">${mood ? 'Edit' : 'Add'} mood</button>  
     </form>
   `
 }
@@ -189,68 +298,49 @@ function generateSuccessPage(mood) {
   `
 }
 
-function handleGotoProfileSubmit() {
-  event.preventDefault(); 
-  $('main').on('click', '#go-to-profile', function(event) {
-    displayProfilePage(); 
-  }); 
-}
+
 
 function displaySuccessPage(mood) { 
   $('main').html(generateSuccessPage(mood));
 } 
 
-function handleShowNewMoodButton(event) {
-  $('main').on('click', '#create-new-mood', function(event) {
-    displayMoodField(); 
-  });
-}
+
 
 //mood input field
-function displayMoodField() { 
-  $('main').html(generateMoodInput()); 
+function displayMoodField(mood = null) { 
+  $('main').html(generateMoodInput(mood)); 
 }
 
 
 //landing page - has link to signin page 
 function generateSignupPage() {
   return `
-    
+      <h1 class="welcome-message">Welcome Back to the Original Mood-Caller</h1>
+      <button type="button" id="showLoginButton" class="button">Login</button> 
       <form class="signup-form"> 
-        <fieldset> 
+        <fieldset class="signup-fieldset"> 
           <legend>Sign Up</legend> 
             <label for="usrname">Username</label> 
             <input type="text" placeholder="Enter email" name="usrname" id="newusername"> 
+            </br>
             <label for="passwrd">Password</label> 
-            <input type="text" placeholder="Enter password" name="passwrd" id="newpassword"> 
-        </fieldset> 
-        <button type="submit" id="register-user">REGISTER</button> 
+            <input type="text" placeholder="Enter password" name="passwrd" id="newpassword">
+            </br> 
+            <button type="submit" id="register-user" class="button">REGISTER</button> 
+        </fieldset>       
       </form> 
-      <button type="button" id="showLoginButton">Login for Users</button> 
   `
 } 
 
-function handleSignupSubmit() { 
-  $('main').on('submit', '.signup-form', function(event) { 
-    event.preventDefault(); 
-    const username = $('#newusername').val(); 
-    const password = $('#newpassword').val(); 
-    const newUser = {username, password}; 
-    addNewUser(newUser, displayProfilePage); 
-  });
-}
+
 
 function addNewUser(newUser, callback) { 
-  STORE.get(callback); 
+  user = newUser; 
+  callback(); 
 }
 
 
-function handleShowLoginButton() {
-  $('main').on('click', '#showLoginButton', function(event) { 
-    event.preventDefault(); 
-    displayLoginPage();
-  });  
-} 
+
 
 function displayLoginPage() {
   $('main').html(generateLoginPage()); 
@@ -269,37 +359,50 @@ function generateLoginPage() {
   `
 } 
 
+let user = null; 
+
+function validateLogin(credentials) { 
+  user = true; 
+  displayLandingPage(); 
+}; 
+
 function handleLoginSubmit() {
   $('main').on('submit', '.login-form', function(event) {
     event.preventDefault(); 
     const username = $('#login-username').val(); 
     const password = $('#login-password').val(); 
-    const user = {username, password};
-    function validateLogin(username, password) { 
-      console.log('Login validated'); 
-    }; 
-    STORE.get(displayProfile); 
+    const credentials = {username, password};
+    validateLogin(credentials); 
+    
   }); 
 }
 
 function displayProfilePage(moods) { 
   $('main').html(generateProfilePage(moods)); 
+  if (moods.length > 0) {
+  generateGraph(moods); 
+  } 
 }
 
-function handleLogoutSubmit() {
-  $('main').on('click', '#log-out', function(event) { 
-    event.preventDefault(); 
-    logout(); 
-  });
-}
+
 
 function logout() { 
-  
+  user = null; 
+  displayLandingPage(); 
 }
 
 function displaySignupPage() {
   $('main').html(generateSignupPage());
 } 
+
+//decides which page to start with
+function displayLandingPage() {
+  if (user) { 
+    getMoodsFromApi(displayProfilePage); 
+  } else {
+    displaySignupPage(); 
+  }
+}
 
 function setupEventHandlers() { 
   handleLoginSubmit(); 
@@ -309,11 +412,13 @@ function setupEventHandlers() {
   handleShowNewMoodButton(); 
   handleMoodSubmit(); 
   handleShowLoginButton(); 
+  handleDelete(); 
+  handleEdit(); 
 }
 
 function setupUi() { 
-  setupEventHandlers(); 
-  displaySignupPage(); 
+  setupEventHandlers();
+  displayLandingPage(); 
 }
 
 $(setupUi);  
---
+++ test/test-entries.js
@@ -0,0 +1,221 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// this makes the should syntax available throughout
+// this module
+const should = chai.should();
+
+const { MoodEntry } = require('../models');
+const { closeServer, runServer, app } = require('../server'); 
+const { TEST_DATABASE_URL } = require('../config'); 
+
+chai.use(chaiHttp); 
+
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+function seedMoodEntryData() {
+  console.info('seeding mood  data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      rating: faker.rating.rating(), 
+      description: faker.description.description(),
+      created: faker.created.created()
+    });
+  }
+  // this will return a promise
+  return MoodEntry.insertMany(seedData);
+}
+
+describe('mood entries API resource', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function () {
+    return seedMoodEntryData();
+  });
+
+  afterEach(function () {
+    // tear down database so we ensure no state from this test
+    // effects any coming after.
+    return tearDownDb();
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
+  // note the use of nested `describe` blocks.
+  // this allows us to make clearer, more discrete tests that focus
+  // on proving something small
+  describe('GET endpoint', function () {
+
+    it('should return all existing posts', function () {
+      // strategy:
+      //    1. get back all posts returned by by GET request to `/posts`
+      //    2. prove res has right status, data type
+      //    3. prove the number of posts we got back is equal to number
+      //       in db.
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.lengthOf.at.least(1);
+
+          return MoodEntry.count();
+        })
+        .then(count => {
+          // the number of returned posts should be same
+          // as number of posts in DB
+          res.body.should.have.lengthOf(count);
+        });
+    });
+
+    it('should return posts with right fields', function () {
+      // Strategy: Get back all posts, and ensure they have expected keys
+
+      let resEntry;
+      return chai.request(app)
+        .get('/entries')
+        .then(function (res) {
+
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.lengthOf.at.least(1);
+
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'title', 'content', 'author', 'created');
+          });
+          // just check one of the posts that its values match with those in db
+          // and we'll assume it's true for rest
+          resEntry = res.body[0];
+          return MoodEntry.findById(resEntry.id);
+        })
+        .then(post => {
+          resEntry.rating.should.equal(post.rating);
+          resEntry.description.should.equal(post.description);
+          resEntry.created.should.equal(post.created);
+        });
+    });
+  });
+
+  describe('POST endpoint', function () {
+    // strategy: make a POST request with data,
+    // then prove that the post we get back has
+    // right keys, and that `id` is there (which means
+    // the data was inserted into db)
+    it('should add a new mood entry', function () {
+
+      const newEntry = {
+        rating: faker.rating(), 
+        description: faker.description(),
+        created: faker.created()
+      };
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newPost)
+        .then(function (res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'rating', 'description', 'created');
+          res.body.rating.should.equal(newEntry.title);
+          // cause Mongo should have created id on insertion
+          res.body.id.should.not.be.null;
+          res.body.description.should.equal(
+            `${newEntry.description}`);
+          res.body.content.should.equal(newPost.content);
+          return BlogPost.findById(res.body.id);
+        })
+        .then(function (post) {
+          post.rating.should.equal(newEntry.rating);
+          post.description.should.equal(newEntry.rating);
+          post.created.should.equal(newEntry.created);
+        });
+    });
+  });
+
+  describe('PUT endpoint', function () {
+
+    // strategy:
+    //  1. Get an existing post from db
+    //  2. Make a PUT request to update that post
+    //  4. Prove post in db is correctly updated
+    it('should update fields you send over', function () {
+      const updateData = {
+        rating: '6', 
+        description: 'I just worked out!', 
+        created: new Date(),
+      };
+
+      return MoodEntry
+        .findOne()
+        .then(post => {
+          updateData.id = post.id;
+
+          return chai.request(app)
+            .put(`/entries/${post.id}`)
+            .send(updateData);
+        })
+        .then(res => {
+          res.should.have.status(204);
+          return BlogPost.findById(updateData.id);
+        })
+        .then(post => {
+          post.rating.should.equal(updateData.rating);
+          post.descriptiong.should.equal(updateData.description);
+          post.created.should.equal(updateData.created);
+        });
+    });
+  });
+
+  describe('DELETE endpoint', function () {
+    // strategy:
+    //  1. get a post
+    //  2. make a DELETE request for that post's id
+    //  3. assert that response has right status code
+    //  4. prove that post with the id doesn't exist in db anymore
+    it('should delete a post by id', function () {
+
+      let post;
+
+      return MoodEntry
+        .findOne()
+        .then(_post => {
+          post = _post;
+          return chai.request(app).delete(`/entries/${post.id}`);
+        })
+        .then(res => {
+          res.should.have.status(204);
+          return MoodEntry.findById(post.id);
+        })
+        .then(_post => {
+          // when a variable's value is null, chaining `should`
+          // doesn't work. so `_post.should.be.null` would raise
+          // an error. `should.be.null(_post)` is how we can
+          // make assertions about a null value.
+          should.not.exist(_post);
+        });
+    });
+  });
+});
