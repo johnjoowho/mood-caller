c685de83ad084107bb26e02c5bdd73e9afc93407
JohnJ <makewavesandsurf@gmail.com>
2018-08-06 18:22:57 -0700

    changed to onlie database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/moods-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://johnjoowho:Wnwlsgur2018@ds255320.mlab.com:55320/mood-app-db';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-moods-app';
 exports.PORT = process.env.PORT || 8080;
 
